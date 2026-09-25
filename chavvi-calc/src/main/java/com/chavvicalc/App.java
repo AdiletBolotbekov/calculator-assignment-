c3af6cf059b3c17afa5db3aca87a4a225705b984
AdiletBolotbekov <154871234+AdiletBolotbekov@users.noreply.github.com>
2026-09-24 20:41:22 -0700

    Add calculator menu

--- chavvi-calc/src/main/java/com/chavvicalc/App.java
+++ chavvi-calc/src/main/java/com/chavvicalc/App.java
@@ -12,6 +12,16 @@ public class App {
         System.out.println("Welcome to ChavviCalc!");
         System.out.println("A = " + a);
         System.out.println("B = " + b);
+        System.out.println();
+        System.out.println("a - Enter a value for A");
+        System.out.println("b - Enter a value for B");
+        System.out.println("+ - Add");
+        System.out.println("- - Subtract");
+        System.out.println("* - Multiply");
+        System.out.println("/ - Divide");
+        System.out.println("= - Display values");
+        System.out.println("c - Clear");
+        System.out.println("q - Quit");
 
         input.close();
     }
