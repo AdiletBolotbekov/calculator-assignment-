fcf10b67b4c8677e409dac36db8a0fd983c6509f
AdiletBolotbekov <154871234+AdiletBolotbekov@users.noreply.github.com>
2026-09-24 20:35:53 -0700

    Initial Maven project setup

---
+++ chavvi-calc/src/main/java/com/chavvicalc/App.java
@@ -0,0 +1,18 @@
+package com.chavvicalc;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double a = 0;
+        double b = 0;
+
+        System.out.println("Welcome to ChavviCalc!");
+        System.out.println("A = " + a);
+        System.out.println("B = " + b);
+
+        input.close();
+    }
+}
