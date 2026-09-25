8e335dbcdb0e2169f41da97ae7b250c7af6d15e5
AdiletBolotbekov <154871234+AdiletBolotbekov@users.noreply.github.com>
2026-09-24 20:45:57 -0700

    Add A and B input commands

--- chavvi-calc/src/main/java/com/chavvicalc/App.java
+++ chavvi-calc/src/main/java/com/chavvicalc/App.java
@@ -8,21 +8,38 @@ public class App {
 
         double a = 0;
         double b = 0;
+        String command = "";
 
-        System.out.println("Welcome to ChavviCalc!");
-        System.out.println("A = " + a);
-        System.out.println("B = " + b);
-        System.out.println();
-        System.out.println("a - Enter a value for A");
-        System.out.println("b - Enter a value for B");
-        System.out.println("+ - Add");
-        System.out.println("- - Subtract");
-        System.out.println("* - Multiply");
-        System.out.println("/ - Divide");
-        System.out.println("= - Display values");
-        System.out.println("c - Clear");
-        System.out.println("q - Quit");
+        while (!command.equals("q")) {
+            System.out.println();
+            System.out.println("Welcome to ChavviCalc!");
+            System.out.println("A = " + a);
+            System.out.println("B = " + b);
+            System.out.println();
+            System.out.println("a - Enter a value for A");
+            System.out.println("b - Enter a value for B");
+            System.out.println("+ - Add");
+            System.out.println("- - Subtract");
+            System.out.println("* - Multiply");
+            System.out.println("/ - Divide");
+            System.out.println("= - Display values");
+            System.out.println("c - Clear");
+            System.out.println("q - Quit");
+            System.out.print("Enter command: ");
 
+            command = input.nextLine();
+
+            if (command.equals("a")) {
+                System.out.print("Enter value for A: ");
+                a = Double.parseDouble(input.nextLine());
+            } 
+            else if (command.equals("b")) {
+                System.out.print("Enter value for B: ");
+                b = Double.parseDouble(input.nextLine());
+            }
+        }
+
+        System.out.println("Goodbye!");
         input.close();
     }
 }
